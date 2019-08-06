1ae2b7b814e607e5ea09566068a4a933e241823a
Jacob Hunt <jacobhuntemail@gmail.com>
2019-08-06 13:05:13 -0400

    Code Style and Documentation
    Fixed typo, made variable names more descriptive, updated comments

--- terminal.py
+++ terminal.py
@@ -18,19 +18,20 @@ class Terminal:
             stdin = subprocess.PIPE,
             stdout = subprocess.PIPE,
             stderr = subprocess.PIPE,
-            shell = True,
+            shell = True
         )
     
     # Execute a bash terminal command (passed as an argument to the function) on the
     # machine running the bot. Returns a string message which can be displayed for the user.
     def executeCommand(self, command):
-        # Execute a shell-terminal command and store the result in "commandReturnObject"
-        out, err = self.terminalInstance.communicate(command)
+        # Execute a shell-terminal command and store the "stdout" and "stderr" output in the
+        # variables "outputString" and "errorString", respectively
+        outputString, errorString = self.terminalInstance.communicate(command)
 
         # String to output to user who executed the BASH command
         return ("The command '" + command + "' was executed in terminal '" + self.name + "'.\n\n"
-      + "The terminal returned the output:\n'" + out + "'.\n\n"
-      + "If there was an error message, it reads: '" + err + "'.\n")
+      + "The terminal returned the output:\n'" + outputString + "'.\n\n"
+      + "If there was an error message, it reads: '" + errorString + "'.\n")
 
 # TEST CODE (for windows):
 # testTerminal = Terminal("test-terminal", "cmd.exe")
